24cc2c04a2d91021b9068f24e2db01ef7d42f9aa
QingGo <zyqingjohn@qq.com>
2018-04-29 03:25:16 +0800

    use while will block the thread, so use setInterval instead

--- zhihu_blockPrivacyPolicy.js
+++ zhihu_blockPrivacyPolicy.js
@@ -6,20 +6,18 @@
 // ==/UserScript==
 
 var __retryTime = 1000;
-var haveDeleted = false;
 
 function __retryFindAndDel(){
-    var __timer = setTimeout(function () {
+	setTimeout(function(){
         var model = document.querySelector('div.Modal-wrapper');
         if (model) {
-        model.remove();
-        document.querySelector('html').style = "";
-        haveDeleted = true;
+            model.remove();
+            document.querySelector('html').style = "";
+            clearInterval(__timer)
         }
-    }, __retryTime);
-    __retryTime *= 2;
+        __retryTime *= 2;
+    		console.log(__retryTime);
+    }, __retryTime)
 }
 
-if (!haveDeleted){
-    __retryFindAndDel();
-}
+var __timer=self.setInterval("__retryFindAndDel();",500)
