26de83aac8fe82841833e11a57d7969f9e32ab06
清歌 <zyqingjohn@qq.com>
2018-04-28 15:31:50 +0800

    renew the logic to delay and retry

--- zhihu_blockPrivacyPolicy.js
+++ zhihu_blockPrivacyPolicy.js
@@ -1,15 +1,25 @@
 // ==UserScript==
-// @name     zhihu block
+// @name     zhihu block privacy policy
 // @version  1
 // @grant    none
 // @match  *://*.zhihu.com/*
 // ==/UserScript==
 
-var __t = 0;
-var __timer = setTimeout(function () {
-var model = document.querySelector('div.Modal-wrapper');
-if (model) {
-model.remove()
-document.querySelector('html').style = ""
+var __retryTime = 1000;
+var haveDeleted = false;
+
+function __retryFindAndDel(){
+	var __timer = setTimeout(function () {
+		var model = document.querySelector('div.Modal-wrapper');
+		if (model) {
+		model.remove();
+		document.querySelector('html').style = "";
+		haveDeleted = true;
+        }
+    }, __retryTime) 
+    __retryTime *= 2;
 }
-}, 3000) 
+
+if (!haveDeleted){
+    __retryFindAndDel();
+}
