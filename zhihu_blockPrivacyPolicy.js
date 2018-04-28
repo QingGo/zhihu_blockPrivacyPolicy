f6b0b0ca0028500b77a3726f5a036ebf64e6eeb1
清歌 <zyqingjohn@qq.com>
2018-04-28 14:26:14 +0800

    first version to block Privacy Policy of zhihu

---
+++ zhihu_blockPrivacyPolicy.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name     zhihu_blockPrivacyPolicy
+// @version  1
+// @grant    none
+// ==/UserScript==
+
+var __t = 0;
+var __timer = setTimeout(function () {
+var model = document.querySelector('body > div:nth-child(7)');
+if (model) {
+model.remove()
+document.querySelector('html').style = ""
+}
+}, 1000) 
